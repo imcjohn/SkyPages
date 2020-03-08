f1cb4532f83e8877cd9224cd0c6a1ce758403f69
Ian McJohn <imcjohn@mit.edu>
2020-03-07 20:23:33 -0500

    starting github page scraping

---
+++ gitscrape/gitTraverse.js
@@ -0,0 +1,8 @@
+/**
+ * Traverse a github page tree, turning it into zip folders along the way
+ * @param url root of git pages
+ * @returns promise of a zip something something
+ */
+function traverse(url){
+
+}
